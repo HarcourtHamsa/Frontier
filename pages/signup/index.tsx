acb8e961138720e32a13e0335af5d7c1004107c9
harcourt hamsa <harcourthamsa@gmail.com>
2022-10-25 13:38:49 +0100

    implements signup ui

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,18 +1,29 @@
+import Link from "next/link";
 import React from "react";
+import Button from "../../components/Button/Button";
 import InputField from "../../components/InputField/InputField";
 import styles from "./signup.module.scss";
 
 const signup = () => {
   return (
-    <div className={styles.login_page}>
+    <div className={styles.signup_page}>
       <div className={styles.card}>
-        <h2>Create your account</h2>
+        <p className={styles.title}>Create your account</p>
         <form>
           <InputField label="Business Name" type="text" />
           <InputField label="First Name" type="text" />
           <InputField label="Last Name" type="text" />
           <InputField label="Email address" type="email" />
+          <InputField label="Password" type="password" />
+          <Button label="Create Account" theme="green" />
         </form>
+
+        <p>
+          Already have an account ?{" "}
+          <Link href={"/login"}>
+            <a>Login</a>
+          </Link>
+        </p>
       </div>
     </div>
   );
